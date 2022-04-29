c695f4ddf2f0d26b2d169651929b462cb9e2a4ef
smarr2198 <smarr2198@gmail.com>
2022-04-29 14:39:45 -0500

    set up indexedDB throught cache and application saves and works offline when server is off

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -42,7 +42,7 @@ export const getDb = async () => {
 
   const result = await request;
   console.log("result.value", result);
-  return result;
+  // return result;
 };
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,7 +20,7 @@ module.exports = () => {
     plugins: [
       new HtmlWebpackPlugin({
         template: "./index.html",
-        title: "Contact Cards",
+        title: "Text Editor",
       }),
 
       new InjectManifest({
@@ -31,9 +31,9 @@ module.exports = () => {
       new WebpackPwaManifest({
         fingerprints: false,
         inject: true,
-        name: "Contact Cards",
-        short_name: "Contact",
-        description: "Never forget your contacts!",
+        name: "Text Editor",
+        short_name: "Text Editor",
+        description: "Text Editor using PWA!",
         background_color: "#225ca3",
         theme_color: "#225ca3",
         start_url: "/",
